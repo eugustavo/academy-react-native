1a41fcf1c17cd4744a2eb6215a70dd1458e7522e
eugustavo <gustavo.ifto@gmail.com>
2022-11-13 18:42:41 -0300

    chore: Create public routes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
@@ -16,7 +18,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,68 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+
+import { useNavigation } from '@react-navigation/native';
+
+import LogoSvg from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png';
+import { Input } from '@components/Form/Input';
+import { Button } from '@components/Button';
+
+export function SignUp() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+
+        <Center my={24}>
+          <LogoSvg />
+
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center mt={6}>
+          <Heading color="gray.100" fontSize="xl" mb="6" fontFamily="heading">
+            Crie sua conta
+          </Heading>
+
+          <Input
+            placeholder='Nome'
+          />
+
+          <Input
+            placeholder='E-mail'
+            keyboardType='email-address'
+            autoCapitalize='none'
+          />
+          
+          <Input
+            placeholder='Senha'
+            secureTextEntry
+            autoCapitalize='none'
+          />
+
+          <Button title='Criar e acessar' />
+        </Center>
+
+
+          <Button mt={20} title='Voltar para o login' variant="outline" onPress={handleGoBack} />
+      </VStack>
+    </ScrollView>
+  );
+}
