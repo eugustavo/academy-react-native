c95ef839c8921bb5ca4e3ca3292ab6cb8eb1132f
eugustavo <gustavo.ifto@gmail.com>
2022-11-14 10:31:34 -0300

    chore: Adjust photo upload

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import * as FileSystem from 'expo-file-system';
 import { TouchableOpacity } from "react-native";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import * as ImagePicker from 'expo-image-picker';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { Avatar } from "@components/Avatar";
@@ -10,7 +12,46 @@ import { Button } from "@components/Button";
 const PHOTO_SIZE = 33;
 
 export function Profile() {
-  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState('https://github.com/eugustavo.png');
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+  
+      if (photoSelected.cancelled) {
+        return
+      }
+
+      if (photoSelected.uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri);
+
+        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          return toast.show({
+            title: 'Ops, imagem muito grande...',
+            description: 'Essa imagem é muito grande. Selecione uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          })
+        }
+
+        setUserPhoto(photoSelected.uri);
+      }
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  };
 
   return (
     <VStack flex={1}>
@@ -28,13 +69,13 @@ export function Profile() {
             />
           ):(
             <Avatar
-              source={{ uri: 'https://github.com/eugustavo.png'}}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
           
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontWeight="bold"
